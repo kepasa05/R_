37632baf3db4f2d59916aa5897c9dcea07d03759
kepasa05 <rromero@andaluciajunta.es>
2020-08-01 01:15:56 +0200

    Añadido comentarios
    Se han añadido comentarios a cada funcion de R que trata al XML

--- scripts/tema1/02_analisis_xml.R
+++ scripts/tema1/02_analisis_xml.R
@@ -4,14 +4,16 @@ install.packages("XML")
 library(XML)
 
 # Carga fichero y position en raiz para comenzar a ver los datos.
-
-url <- "../data/cd_catalog.xml"
-xmldoc <- xmlParse(url)
-rootnode <- xmlRoot(xmldoc)
-rootnode[1]
+url <- "../data/cd_catalog.xml" 
+xmldoc <- xmlParse(url)        # devuelve un XML Internal Document
+rootnode <- xmlRoot(xmldoc)    # Accede al nodo raiz del xml, obteniendo la estructura de datos.
+rootnode[1]                    
 
 # creacion dataframe de Cd
-csd_data <- xmlSApply(rootnode,function(x) xmlSApply(x,xmlValue))
+# xmlSApply recorre todo el nodo obteniedo sus valores. raiz (xmlValue)
+csd_data <- xmlSApply(rootnode,function(x) xmlSApply(x,xmlValue)) 
 
 # Transposicion
 cds.catalog <- data.frame(t(csd_data), row.names = NULL)
+head(cds.catalog, 2)
+cds.catalog[1:5,]
